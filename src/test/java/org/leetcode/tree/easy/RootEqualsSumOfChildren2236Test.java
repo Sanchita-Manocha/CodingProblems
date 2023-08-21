a6ba82ced6c9933472bf06d9ccdba527120f7da1
Sanchita Manocha <sanchita@Sanchitas-MacBook-Pro.local>
2023-08-21 23:31:18 +1000

    solution for Problem 2236

---
+++ src/test/java/org/leetcode/tree/easy/RootEqualsSumOfChildren2236Test.java
@@ -0,0 +1,28 @@
+package org.leetcode.tree.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RootEqualsSumOfChildren2236Test {
+
+    @Test
+    public void testcase1(){
+        RootEqualsSumOfChildren2236 sut =new RootEqualsSumOfChildren2236();
+        RootEqualsSumOfChildren2236.TreeNode treeNodeLeft = new RootEqualsSumOfChildren2236.TreeNode(4);
+        RootEqualsSumOfChildren2236.TreeNode treeNodeRight = new RootEqualsSumOfChildren2236.TreeNode(6);
+        RootEqualsSumOfChildren2236.TreeNode root = new RootEqualsSumOfChildren2236.TreeNode(10,treeNodeLeft,treeNodeRight);
+
+        assertTrue(sut.checkTree(root));
+    }
+    @Test
+    public void testcase2(){
+        RootEqualsSumOfChildren2236 sut =new RootEqualsSumOfChildren2236();
+        RootEqualsSumOfChildren2236.TreeNode treeNodeLeft = new RootEqualsSumOfChildren2236.TreeNode(3);
+        RootEqualsSumOfChildren2236.TreeNode treeNodeRight = new RootEqualsSumOfChildren2236.TreeNode(1);
+        RootEqualsSumOfChildren2236.TreeNode root = new RootEqualsSumOfChildren2236.TreeNode(5,treeNodeLeft,treeNodeRight);
+
+        assertFalse(sut.checkTree(root));
+    }
+
+}
