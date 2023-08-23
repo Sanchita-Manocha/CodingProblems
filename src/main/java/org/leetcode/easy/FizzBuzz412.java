32958b3c38861bfdd64d7e6e11e79908958ff7ab
sanchita <sanchita.manocha85@gmail.com>
2023-08-23 23:45:24 +1000

    [412]: Fizz Buzz

---
+++ src/main/java/org/leetcode/easy/FizzBuzz412.java
@@ -0,0 +1,35 @@
+package org.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz412 {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0)
+                result.add("FizzBuzz");
+            else if (i % 3 == 0)
+                result.add("Fizz");
+            else if (i % 5 == 0)
+                result.add("Buzz");
+            else
+                result.add(String.valueOf(i));
+        }
+        return result;
+    }
+    public List<String> fizzBuzz1(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            String resultString ="";
+            if (i % 3 == 0)
+                resultString+="Fizz";
+            if (i % 5 == 0)
+                resultString+="Buzz";
+            if(resultString.isEmpty())
+                resultString+=String.valueOf(i);
+            result.add(resultString);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/org/leetcode/easy/FizzBuzz412Test.java
@@ -0,0 +1,32 @@
+package org.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzz412Test {
+    FizzBuzz412 sut = new FizzBuzz412();
+
+    @Test
+    public void test1(){
+        List<String> expected = List.of("1","2","Fizz");
+        assertEquals(expected, sut.fizzBuzz(3));
+        assertEquals(expected, sut.fizzBuzz1(3));
+    }
+@Test
+    public void test2(){
+        List<String> expected = List.of("1","2","Fizz","4","Buzz");
+        assertEquals(expected, sut.fizzBuzz(5));
+        assertEquals(expected, sut.fizzBuzz1(5));
+    }
+
+    @Test
+    public void test3() {
+        List<String> expected = List.of("1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz");
+        assertEquals(expected, sut.fizzBuzz(15));
+        assertEquals(expected, sut.fizzBuzz1(15));
+    }
+
+}
