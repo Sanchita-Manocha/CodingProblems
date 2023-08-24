a220ed8710d7801db921110b0e3010cfa366a318
sanchita <sanchita.manocha85@gmail.com>
2023-08-24 23:41:57 +1000

    cleanup

--- src/main/java/org/leetcode/easy/tree/RootEqualsSumOfChildren2236.java
+++ src/main/java/org/leetcode/easy/tree/RootEqualsSumOfChildren2236.java
@@ -26,3 +26,13 @@ public class RootEqualsSumOfChildren2236 {
     }
 }
 
+    git rm -f .gradle/8.2/executionHistory/executionHistory.bin
+        git rm -f .gradle/8.2/executionHistory/executionHistory.lock
+        git rm -f .gradle/8.2/fileHashes/fileHashes.bin
+        git rm -f .gradle/8.2/fileHashes/fileHashes.lock
+        git rm -f .gradle/8.2/fileHashes/resourceHashesCache.bin
+        git rm -f .gradle/buildOutputCleanup/buildOutputCleanup.lock
+        git rm -f build/reports/tests/test/classes/org.leetcode.tree.easy.RootEqualsSumOfChildren2236Test.html
+        git rm -f build/reports/tests/test/packages/org.leetcode.tree.easy.html
+        git rm -f build/test-results/test/TEST-org.leetcode.tree.easy.RootEqualsSumOfChildren2236Test.xml
+        git rm -f build/tmp/compileTestJava/compileTransaction/stash-dir/RootEqualsSumOfChildren2236Test.class.uniqueId0
