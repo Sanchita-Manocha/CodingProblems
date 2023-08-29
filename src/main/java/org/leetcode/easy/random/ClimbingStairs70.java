7b8606e6fe96a00a087d7249369fcbb6208f7353
sanchita <sanchita.manocha85@gmail.com>
2023-08-30 00:37:47 +1000

    [70]: DP climb stairs

---
+++ src/main/java/org/leetcode/easy/random/ClimbingStairs70.java
@@ -0,0 +1,28 @@
+package org.leetcode.easy.random;
+
+public class ClimbingStairs70 {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public int climbStairs1(int n) {
+        int[] memo = new int[n + 1];
+        return climbStairs1(n, memo);
+    }
+
+    public int climbStairs1(int n, int[] memo) {
+        if (n == 1 || n == 2)
+            return n;
+        if (memo[n] == 0) {
+            memo[n] = climbStairs1(n - 1, memo) + climbStairs1(n - 2, memo);
+        }
+        return memo[n];
+
+    }
+}
---
+++ src/test/java/org/leetcode/easy/random/ClimbingStairs70Test.java
@@ -0,0 +1,26 @@
+package org.leetcode.easy.random;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClimbingStairs70Test {
+    ClimbingStairs70 sut = new ClimbingStairs70();
+
+    @Test
+    public void test1(){
+        assertEquals(2, sut.climbStairs(2));
+        assertEquals(2, sut.climbStairs1(2));
+    }
+    @Test
+    public void test2(){
+        assertEquals(3, sut.climbStairs(3));
+        assertEquals(3, sut.climbStairs1(3));
+    }
+    @Test
+    public void test3(){
+        assertEquals(1, sut.climbStairs(1));
+        assertEquals(1, sut.climbStairs1(1));
+    }
+
+}
